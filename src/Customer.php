f47c04401cc141f403cc8e440bac2b658636e741
Sam Nowakowski <samnowi@me.com>
2022-06-05 12:05:34 +0200

    Extract method

--- src/Customer.php
+++ src/Customer.php
@@ -28,25 +28,9 @@ class Customer
         $result = 'Rental Record for ' . $this->getName() . "\n";
 
         for ($i = 0; $i < count($this->_rentals); $i++) {
-            $thisAmount = 0;
             $each = $this->_rentals[$i];
 
-            // determine amounts for each line
-            switch ($each->getMovie()->getPriceCode()) {
-                case Movie::REGULAR:
-                    $thisAmount += 2;
-                    if ($each->getDaysRented() > 2)
-                        $thisAmount += ($each->getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie::NEW_RELEASE:
-                    $thisAmount += $each->getDaysRented() * 3;
-                    break;
-                case Movie::CHILDREN:
-                    $thisAmount += 1.5;
-                    if ($each->getDaysRented() > 3)
-                        $thisAmount += ($each->getDaysRented() - 3) * 1.5;
-                    break;
-            }
+            $thisAmount = $this->amountFor($each);
 
             // add frequent renter points
             $frequentRenterPoints++;
@@ -65,5 +49,27 @@ class Customer
         $result .= 'You earned ' . $frequentRenterPoints . ' frequent renter points';
 
         return $result;
+    }
+
+    private function amountFor(Rental $each): float {
+        $thisAmount = 0;
+
+        switch ($each->getMovie()->getPriceCode()) {
+            case Movie::REGULAR:
+                $thisAmount += 2;
+                if ($each->getDaysRented() > 2)
+                    $thisAmount += ($each->getDaysRented() - 2) * 1.5;
+                break;
+            case Movie::NEW_RELEASE:
+                $thisAmount += $each->getDaysRented() * 3;
+                break;
+            case Movie::CHILDREN:
+                $thisAmount += 1.5;
+                if ($each->getDaysRented() > 3)
+                    $thisAmount += ($each->getDaysRented() - 3) * 1.5;
+                break;
+        }
+
+        return $thisAmount;
     }
 }
