51afa44cade2409868dbac59d230657cf247c4b4
Sam Nowakowski <samnowi@me.com>
2022-06-05 12:07:05 +0200

    Rename local variable

--- src/Customer.php
+++ src/Customer.php
@@ -51,22 +51,22 @@ class Customer
         return $result;
     }
 
-    private function amountFor(Rental $each): float {
+    private function amountFor(Rental $rental): float {
         $thisAmount = 0;
 
-        switch ($each->getMovie()->getPriceCode()) {
+        switch ($rental->getMovie()->getPriceCode()) {
             case Movie::REGULAR:
                 $thisAmount += 2;
-                if ($each->getDaysRented() > 2)
-                    $thisAmount += ($each->getDaysRented() - 2) * 1.5;
+                if ($rental->getDaysRented() > 2)
+                    $thisAmount += ($rental->getDaysRented() - 2) * 1.5;
                 break;
             case Movie::NEW_RELEASE:
-                $thisAmount += $each->getDaysRented() * 3;
+                $thisAmount += $rental->getDaysRented() * 3;
                 break;
             case Movie::CHILDREN:
                 $thisAmount += 1.5;
-                if ($each->getDaysRented() > 3)
-                    $thisAmount += ($each->getDaysRented() - 3) * 1.5;
+                if ($rental->getDaysRented() > 3)
+                    $thisAmount += ($rental->getDaysRented() - 3) * 1.5;
                 break;
         }
 
