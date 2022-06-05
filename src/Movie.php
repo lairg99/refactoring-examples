b2a083efc65eb268f1155560f96ae318b82986f9
Sam Nowakowski <samnowi@me.com>
2022-06-05 12:19:57 +0200

    Move and delegate method

--- src/Movie.php
+++ src/Movie.php
@@ -27,4 +27,26 @@ class Movie
     public function getTitle(): string {
         return $this->_title;
     }
+
+    public function getCharge(int $daysRented): float {
+        $thisAmount = 0;
+
+        switch ($this->getPriceCode()) {
+            case Movie::REGULAR:
+                $thisAmount += 2;
+                if ($daysRented > 2)
+                    $thisAmount += ($daysRented - 2) * 1.5;
+                break;
+            case Movie::NEW_RELEASE:
+                $thisAmount += $daysRented * 3;
+                break;
+            case Movie::CHILDREN:
+                $thisAmount += 1.5;
+                if ($daysRented > 3)
+                    $thisAmount += ($daysRented - 3) * 1.5;
+                break;
+        }
+
+        return $thisAmount;
+    }
 }
