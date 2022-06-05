61bf35c6bfb13a75d7bcaa400556bed2266305e0
Sam Nowakowski <samnowi@me.com>
2022-06-05 12:58:52 +0200

    Extract method, replaced foreach mapping by collection

--- src/Printer/Console.php
+++ src/Printer/Console.php
@@ -2,22 +2,30 @@
 
 namespace App\Printer;
 
-use App\Customer;
-
 class Console extends Printer
 {
     public function print(): string {
-        $result = 'Rental Record for ' . $this->customer->getName() . "\n";
-
-        foreach ($this->customer->getRentals() as $rental) {
-            // show figures for this rental
-            $result .= "\t" . $rental->getMovie()->getTitle() . "\t" . $rental->getCharge() . "\n";
-        }
-
-        // add footer lines
-        $result .= 'Amount owed is ' . $this->customer->getTotalCharge() . "\n";
-        $result .= 'You earned ' . $this->customer->getTotalFrequentRenterPoints() . ' frequent renter points';
+        $result = $this->header();
+        $result .= $this->overview();
+        $result .= $this->footer();
 
         return $result;
+    }
+
+    private function header(): string {
+        return 'Rental Record for ' . $this->customer->getName() . "\n";
+    }
+
+    private function footer(): string {
+        $footer = 'Amount owed is ' . $this->customer->getTotalCharge() . "\n";
+        $footer .= 'You earned ' . $this->customer->getTotalFrequentRenterPoints() . ' frequent renter points';
+
+        return $footer;
+    }
+
+    private function overview(): string {
+        return $this->customer->getRentals()->map(
+            fn($rental) => "\t" . $rental->getMovie()->getTitle() . "\t" . $rental->getCharge() . "\n"
+        )->join('');
     }
 }
